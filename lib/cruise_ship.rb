cf1e03d00c2cbd1fec3d993bc9f008628c69c34d
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-27 19:18:16 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,13 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
   winner = ""
   passengers.each do |suite, name|
-    name.start_with?("A")
-    winner = name
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
   end
-  winner
+   
+  winner 
+  
 end
